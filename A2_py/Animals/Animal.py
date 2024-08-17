26c2da8482a7aed81389095a6f8a94d47621e743
NicoNL <a0t4zr@inf.elte.hu>
2024-08-17 22:17:34 +0200

    A2 in python started

---
+++ A2_py/Animals/Animal.py
@@ -0,0 +1,13 @@
+class Animal(ABC):
+    def __init__(self, name : str, ex: int, type: str):
+        self.name = name
+        self.ex =  ex
+        self.type = type
+
+    def getName():
+        return self.name
+    def getEx():
+        return self.ex
+    def getType():
+        return self.type
+         
