8fe5d75db154b68f2a7ad721ab56ea2f7486f2e5
NicoNL <a0t4zr@inf.elte.hu>
2024-08-18 20:11:21 +0200

    Methods were added

--- A2_py/Animals/Animal.py
+++ A2_py/Animals/Animal.py
@@ -1,13 +1,39 @@
 class Animal(ABC):
+    class DeadAnimalException(Exception):
+        pass
+    class LimitExhilarationException(Exception):
+        pass
+
     def __init__(self, name : str, ex: int, type: str):
         self.name = name
         self.ex =  ex
         self.type = type
 
-    def getName():
+    def changeEx(self):
+        pass
+    def getName(self):
         return self.name
-    def getEx():
+    def getEx(self):
         return self.ex
-    def getType():
+    def getType(self):
         return self.type
-         
+    def _decreaseEX(self, x):
+        if self.ex == 0:
+            raise Animal.DeadAnimalException(f"{self.type}:{self.name} has died")
+        if self.ex  - x < 0:
+            self.ex = 0
+        else:
+            self.ex -= x
+    def _increaseEx(self,x):
+        if self.ex == 70:
+            raise Animal.LimitExhilarationException(f"{self.type}:{self.name} has died")
+        if self.ex == 0:
+            raise Animal.DeadAnimalException(f"{self.type}:{self.name} has died")
+        if self.ex + x  > 70:
+            self.ex = 70
+        else:
+            self.ex += x
+    
+    def __str(self):
+        return f"{self.type}:{self.name}"
+        
