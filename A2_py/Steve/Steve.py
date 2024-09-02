1653f530e5bb9f3e20c68cf554eb0c06490ecb06
NicoNL <a0t4zr@inf.elte.hu>
2024-09-02 21:35:28 +0200

    Steve class: Methods were added

---
+++ A2_py/Steve/Steve.py
@@ -0,0 +1,10 @@
+class Steve:
+    def __init__(self,animals : list[Animal], Mood : mood):
+        self.__animals =  animals
+        self.__mood = mood
+    def setMood(self, Mood: mood):
+        self.__mood = mood
+
+
+
+
