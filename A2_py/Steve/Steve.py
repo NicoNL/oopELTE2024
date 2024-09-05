51a169fb658ab613d058bc2a6d8eec05266b1495
NicoNL <a0t4zr@inf.elte.hu>
2024-09-05 20:00:23 +0200

    Class steves terminated

--- A2_py/Steve/Steve.py
+++ A2_py/Steve/Steve.py
@@ -2,6 +2,7 @@ class Steve:
     def __init__(self,animals : list[Animal], Mood : mood):
         self.__animals =  animals
         self.__mood = mood
+        self.__bestAnimals = []
     def setMood(self, Mood: mood):
         self.__mood = mood
     def improvableDay(self):
@@ -19,7 +20,21 @@ class Steve:
                 pass
             finally:
                 pass
-        
+    def biggestEx(self):
+        self.__bestAnimals = []
+        maxAnimal = self.__animals[0]
+        max =  maxAnimal.getEx()
+        self.__bestAnimals.append(maxAnimal)
+        for i in range (1, len(self.__animals)):
+            if animals[i].getEx() > max:
+                self.__bestAnimals = []
+                max =  animals[i].getEx()
+                maxAnimal = animals[i]
+                self.__bestAnimals.add(maxAnimal)
+            elif animals[i].getEx() == max:
+                self.__bestAnimals.append(animals[i])
+        return self.__bestAnimals
+
 
 
 
